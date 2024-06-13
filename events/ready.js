d8a45380ec13b318b9e3432cea8bf9e6fea98076
jasperdjy <ggrggegwrwg@gmail.com>
2024-06-13 10:24:00 +0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -46,7 +46,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const totalMembers = client.guilds.cache.reduce((total, guild) => total + guild.memberCount, 0);
 setInterval(() => client.user.setActivity({ 
-  name:`Cʜɪʟʟ Cʜʀᴏɴɪᴄʟᴇs`, 
+  name:`crackthegames.com`, 
   type: ActivityType.streaming }), 10000);
 client.errorLog = config.errorLog
   
